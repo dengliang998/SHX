bf2cbcc880d7a78f07172b8961864c89399245c6
dengliang <dengliang@esbpm.com>
2026-09-01 15:23:51 +0800

    ci: avoid test macro dependency in release builds

--- Package.swift
+++ Package.swift
@@ -1,6 +1,44 @@
 // swift-tools-version: 6.0
 
 import PackageDescription
+import Foundation
+
+let isReleaseBuild = ProcessInfo.processInfo.environment["KITESHELL_RELEASE_BUILD"] == "1"
+
+var packageDependencies: [Package.Dependency] = [
+    .package(url: "https://github.com/migueldeicaza/SwiftTerm.git", exact: "1.15.0")
+]
+
+var packageTargets: [Target] = [
+    .executableTarget(
+        name: "RemoteHub",
+        dependencies: [
+            .product(name: "SwiftTerm", package: "SwiftTerm")
+        ],
+        path: "Sources/RemoteHub"
+    ),
+    .executableTarget(
+        name: "KiteShellUpdater",
+        path: "Sources/KiteShellUpdater"
+    )
+]
+
+if !isReleaseBuild {
+    packageDependencies.append(
+        .package(url: "https://github.com/swiftlang/swift-testing.git", exact: "6.2.3")
+    )
+    packageTargets.append(
+        .testTarget(
+            name: "RemoteHubTests",
+            dependencies: [
+                "RemoteHub",
+                .product(name: "SwiftTerm", package: "SwiftTerm"),
+                .product(name: "Testing", package: "swift-testing")
+            ],
+            path: "Tests/RemoteHubTests"
+        )
+    )
+}
 
 let package = Package(
     name: "KiteShell",
@@ -11,30 +49,6 @@ let package = Package(
         .executable(name: "KiteShell", targets: ["RemoteHub"]),
         .executable(name: "KiteShellUpdater", targets: ["KiteShellUpdater"])
     ],
-    dependencies: [
-        .package(url: "https://github.com/migueldeicaza/SwiftTerm.git", exact: "1.15.0"),
-        .package(url: "https://github.com/swiftlang/swift-testing.git", exact: "6.2.3")
-    ],
-    targets: [
-        .executableTarget(
-            name: "RemoteHub",
-            dependencies: [
-                .product(name: "SwiftTerm", package: "SwiftTerm")
-            ],
-            path: "Sources/RemoteHub"
-        ),
-        .executableTarget(
-            name: "KiteShellUpdater",
-            path: "Sources/KiteShellUpdater"
-        ),
-        .testTarget(
-            name: "RemoteHubTests",
-            dependencies: [
-                "RemoteHub",
-                .product(name: "SwiftTerm", package: "SwiftTerm"),
-                .product(name: "Testing", package: "swift-testing")
-            ],
-            path: "Tests/RemoteHubTests"
-        )
-    ]
+    dependencies: packageDependencies,
+    targets: packageTargets
 )
